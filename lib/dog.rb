1d9d056babf6c618f434e3fc85467f1d004df4cb
Hanna Bergland <berglandhanna@gmail.com>
2019-07-17 17:40:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog 
+  def name=(d_name)
+    @new_d_name = d_name
   
-end  # Your code goes here!
+end
+end  
+
+
+# Your code goes here!
+=begin class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+=end 
